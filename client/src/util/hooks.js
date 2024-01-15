00579394ef83189c362442aff875e3c27f10f98f
mjedwards <jairedwards20@gmail.com>
2024-01-15 15:45:06 -0500

    completed the login functionality, improved the useUserForm hook to use input validation for emails. Also, improved the backend login function to find a user based on their email or username.

--- client/src/util/hooks.js
+++ client/src/util/hooks.js
@@ -1,8 +1,16 @@
 import { useState } from "react";
 
-export const userForm = (callback, initialState = {}) => {
+export const useUserForm = (callback, initialState = {}) => {
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const [values, setValues] = useState(initialState);
+  const [formErrors, setFormErrorsErrors] = useState({});
+
+  const validateEmail = (email) => {
+    // Regular expression to validate email
+    const re = /^(([^<>()\[\]\\.,;:\s@\"]+(\.[^<>()\[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email.toLowerCase());
+  };
+  
 
   const onChange = (event) => {
     setValues({ ...values, [event.target.name]: event.target.value });
@@ -10,6 +18,12 @@ export const userForm = (callback, initialState = {}) => {
 
   const onSubmit = (event) => {
     event.preventDefault();
+    if ('email_username' in values && values.email_username.includes('@')) {
+      if (!validateEmail(values.email_username)) {
+        setFormErrorsErrors({ ...formErrors, email_username: 'Invalid email format' });
+        return; // Stop the form submission if validation fails
+      }
+    }
     callback();
   };
 
@@ -17,5 +31,6 @@ export const userForm = (callback, initialState = {}) => {
     onChange,
     onSubmit,
     values,
+    formErrors,
   };
 };
