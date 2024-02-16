9cec4f54b924d8cd1125c8dfa8fd6475138c4455
mjedwards <jairedwards20@gmail.com>
2024-02-16 16:10:59 -0500

    fixed the drag and drop feature on the task cards. Also, the styling was adjusted

--- client/src/user/Tasks/Task/IndividualTask.js
+++ client/src/user/Tasks/Task/IndividualTask.js
@@ -13,7 +13,7 @@ const Task = ({ task, index, onDragStart }) => {
 			onDragStart={handleDragStart}
 			// className='cursor-pointer border border-gray-200 rounded-lg p-4 shadow-md w-full sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/3 px-4 mb-4 task-card'
             className="w-64 h-64 m-4 bg-white border border-gray-300 rounded-lg cursor-pointer transition duration-200 ease-in-out hover:shadow-md"
-			style={{ width: "200px", height: "200px" }}>
+			style={{ width: "250px", height: "250px" }}>
 			<h1 className='text-lg font-semibold'>{title}</h1>
 		</div>
 	);
