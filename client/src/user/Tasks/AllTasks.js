1e045112216d733b567dddca63683575bb53b06b
mjedwards <jairedwards20@gmail.com>
2024-02-19 20:34:23 -0500

    improved the sorting of tasks

--- client/src/user/Tasks/AllTasks.js
+++ client/src/user/Tasks/AllTasks.js
@@ -1,21 +1,28 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import Task from "./Task/IndividualTask";
+import Sortable from 'sortablejs';
+
 
 const TaskList = ({
 	tasks,
-	onTaskDrop,
 	onDragStart,
-	onDragOver,
-	onDragEnter,
-	onDragLeave,
 }) => {
+    const sortableContainer = useRef(null);
+
+    useEffect(() => {
+      if (sortableContainer.current) {
+        Sortable.create(sortableContainer.current, {
+          animation: 150,
+          ghostClass: 'sortable-ghost',
+          
+        });
+      }
+    }, []);
+
 	return (
 		<div
-			className='flex flex-wrap gap-4 task-list overflow-y-scroll h-screen'
-			onDrop={onTaskDrop}
-			onDragOver={onDragOver}
-			onDragEnter={onDragEnter}
-			onDragLeave={onDragLeave}
+			className='flex flex-wrap gap-4 task-list overflow-y-scroll h-screen sortable-container'
+            ref={sortableContainer}
 		>
 			{tasks.map((task) => (
 				<Task key={task.id} task={task} onDragStart={onDragStart} />
